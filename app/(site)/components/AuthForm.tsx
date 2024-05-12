d9ef17c2d713615725f73dd140ba157fe1098a2b
sandy <81100360+sr-dev-coder@users.noreply.github.com>
2024-05-12 14:05:50 +0530

    setup messenger clone and created login and register funcnality

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,139 @@
+'use client'
+import axios from "axios";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/input";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitErrorHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
+type variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<variant>('LOGIN');
+  const [loading, setLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if(variant === 'LOGIN'){
+        setVariant('REGISTER')
+    }else {
+        setVariant('LOGIN')
+    }
+  }, [variant]);
+
+  const { register, handleSubmit, formState:{ errors }} = useForm<FieldValues>({
+    defaultValues: {
+        name: '',
+        email: '',
+        password: ''
+    }
+  })
+
+  const onSubmit: SubmitErrorHandler<FieldValues> = (data) => {
+    setLoading(true);
+    if(variant === 'REGISTER'){
+        //Axios Register
+        axios.post('/api/register', data)
+        .catch(() => toast.error('Something went wrong!'))
+        .finally(() => setLoading(false))
+    }
+    if(variant === 'LOGIN'){
+        //Axios Login
+        signIn('credentials', {
+            ...data,
+            redirect: false
+        })
+        .then((callback) => {
+            if(callback?.error){
+                toast.error('Invalid credentials')
+            }
+            if(callback?.ok && !callback?.error){
+                toast.success('logged in!')
+            }
+        })
+        .finally(() => setLoading(false))
+    }
+
+    const socialAction = (action: string) => {
+        setLoading(true);
+    }
+  }
+  return (
+    <div
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+    >
+        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+            <form
+                className="space-y-6"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                {
+                    variant === 'REGISTER' && (
+                        <Input 
+                            id="name"
+                            label="Name" 
+                            register={register} 
+                            errors={errors}
+                         />
+                    )
+                }
+                <Input 
+                    id="email"
+                    label="Email Address"
+                    type="email" 
+                    register={register} 
+                    errors={errors}
+                />
+                <Input 
+                    id="password"
+                    label="Password"
+                    type="password" 
+                    register={register} 
+                    errors={errors}
+                />
+                <div>
+                    <Button
+                        fullWidth
+                        disabled={loading}
+                        type="submit"
+                    >
+                         {variant === 'LOGIN' ? 'Sign In' : 'Register'}  
+                    </Button>
+                </div>
+            </form>    
+            <div className="mt-6">
+                <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <div className="w-full border-t border-gray-300" />
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                        <span className="bg-white px-2 text-gray-500">or continue with</span>
+                    </div>
+                </div>
+                <div className="mt-6 flex gap-2">
+                    <AuthSocialButton 
+                        Icon={BsGithub} 
+                        onClick={() => socialAction('github')}
+                     />
+                     <AuthSocialButton 
+                        Icon={BsGoogle} 
+                        onClick={() => socialAction('google')}
+                     />
+                </div>
+            </div>
+            <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                <div>
+                    {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account'}
+                </div>
+                <div onClick={toggleVariant} className="underline cursor-pointer">
+                    {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AuthForm
---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,19 @@
+import { IconType } from "react-icons"
+
+interface AuthSocialButtonProps{
+    Icon: IconType,
+    onClick: () => void
+}
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({ Icon, onClick }) => {
+  return (
+    <button
+        type="button"
+        onClick={onClick}
+        className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+    >
+        <Icon />
+    </button>
+  )
+}
+
+export default AuthSocialButton
