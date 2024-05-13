eb385e0574948388273be7dc7fb495f247f960c6
sandy <81100360+sr-dev-coder@users.noreply.github.com>
2024-05-14 00:16:47 +0530

    Added users list

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,17 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UsersList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const users = await getUsers();
   return (
     <Sidebar>
         <div className="h-full">
+            <UsersList items={users} />
             {children}
         </div>
     </Sidebar>
