051cc127bd0771fbe35db8caaa1ff59105a3798b
sandy <81100360+sr-dev-coder@users.noreply.github.com>
2024-05-14 19:01:02 +0530

    added user image

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from "next/image"
 import { User } from '@prisma/client'
 
 interface AvatarProps{
@@ -6,9 +7,14 @@ interface AvatarProps{
 }
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  console.log(user);
   return (
     <div className='relative'>
         <div className="relative bg-gray-400 inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
+          {
+            user?.image && (<Image src={user?.image as string} alt="" fill />)
+          }
+            
         </div>
             <span className='absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3'></span>
     </div>
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     experimental:{
         swcPlugins: [["next-superjson-plugin", {}]]
+    },
+    images:{
+        domains: ['avatars.githubusercontent.com']
     }
 };
 
